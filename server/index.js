9e9b73b560acb82cb1ec79dd74b6eb1c0885571c
David Diaz <diazdavid.info@gmail.com>
2021-05-09 12:06:07 +0200

    Versioning API

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const requestId = require('express-request-id')();
 
 const logger = require('./config/logger');
-const api = require('./api')
+const api = require('./api/v1')
 
 const app = express();
 
@@ -10,6 +10,7 @@ app.use(requestId);
 app.use(logger.requests);
 
 app.use('/api', api);
+app.use('/api/v1', api);
 
 app.use((req, res, next) => {
     const message = 'Error. Router not found';
