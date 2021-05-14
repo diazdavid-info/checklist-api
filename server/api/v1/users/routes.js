7db762c1ec6b72e3e076bb8eab04226b447a23ac
David Diaz <diazdavid.info@gmail.com>
2021-05-14 21:49:20 +0200

    Add Groups and Users resources

---
+++ server/api/v1/users/routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const controller = require('./controller');
+
+router.route('/')
+    .get(controller.all)
+    .post(controller.create);
+
+router.param('id', controller.id);
+
+router.route('/:id')
+    .get(controller.read)
+    .put(controller.update)
+    .delete(controller.delete);
+
+module.exports = router;
